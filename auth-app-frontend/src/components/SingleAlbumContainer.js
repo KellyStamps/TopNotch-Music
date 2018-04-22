d4d9a27682a40268bafc2e490e1cb61558277db3
KellyStamps <kellydsample@gmail.com>
2018-04-22 16:53:05 -0400

    added modal when favorite is added

--- auth-app-frontend/src/components/SingleAlbumContainer.js
+++ auth-app-frontend/src/components/SingleAlbumContainer.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
+import Modal from 'react-responsive-modal'
 import FavoritesAdapter from '../api/FavoritesAdapter'
 import {addFave} from '../actions/users'
+import './App.css'
+
+const divStyle = {
+  margin: '40px',
+  border: '5px solid pink'
+};
 
 class SingleAlbumContainer extends React.Component {
+
+  state = {
+    open: false
+  }
 
   addFave = () => {
     FavoritesAdapter.saveFavorite(this.props.album, this.props.user)
     .then(res => res.json())
     .then(json => this.props.addFave(json.new_fave))
+
+    this.onOpenModal()
+  }
+
+  onOpenModal = () => {
+    this.setState({open: true})
+  }
+
+  onCloseModal = () => {
+    this.setState({open: false})
   }
 
   render () {
@@ -23,6 +44,11 @@ class SingleAlbumContainer extends React.Component {
           </div>
           <img src={this.props.album.image[3]['#text']}/>
           <button onClick={this.addFave} className='button fave-button'>Add to Favorites</button>
+
+          <Modal open={this.state.open} onClose={this.onCloseModal} little>
+            <h2>Favorite added</h2>
+          </Modal>
+
         </div>
       )
     }
